bcd4328e60f520a06830955f0bb4d35336c5abd5
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2023-08-18 06:38:30 +0100

    changing variables to proper names for other version of concurrency -AP

--- multiple_read_from_chan.go
+++ multiple_read_from_chan.go
@@ -4,12 +4,13 @@ import "sync"
 
 type PubSubConCur2 struct {
 	Mutex       sync.RWMutex
-	Subscribers map[string][]chan string
+	Subscribers map[string]chan string
 }
 
-func NewPubSubConCur2() *PubSubConCur {
-	return &PubSubConCur{
-		mutex:       sync.RWMutex{},
-		Subscribers: make(map[string][]chan string),
+func NewPubSubConCur2() *PubSubConCur2 {
+	return &PubSubConCur2{
+		Mutex:       sync.RWMutex{},
+		Subscribers: make(map[string]chan string),
 	}
+}
 }
