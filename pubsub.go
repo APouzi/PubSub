c2de69cfa4422d563aac03b16ec0a5d0032b2b48
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2023-08-18 06:09:59 +0100

    adding comments for explanation of some pubsub stuff -AP

--- pubsub.go
+++ pubsub.go
@@ -10,10 +10,12 @@ func NewPubSub() *SubPub {
 	}
 }
 
+// The function that is passed in (closure) is the representation of the subscriber.
 func (pb *SubPub) Subscribe(topic string, fn func(string)) {
 	pb.Subscribers[topic] = append(pb.Subscribers[topic], fn)
 }
 
+// Looping over the topic's subscribers and sending off the message
 func (pb *SubPub) Publish(topic string, message string) {
 	for _, fnHnd := range pb.Subscribers[topic] {
 		fnHnd(message)
