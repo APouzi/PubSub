0384bfdf17c56e165557cf5562c70480d8006829
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2023-08-18 06:04:16 +0100

    adding main.go - AP

---
+++ main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"sync"
+	"time"
+)
+
+func main() {
+
+	// Sequintial Version
+	PB := NewPubSub()
+	PB.Subscribe("tech", func(msg string) {
+		fmt.Println("sub1 recieved message:" + msg)
+	})
+	PB.Subscribe("cars", func(msg string) {
+		fmt.Println("sub2 recieved message:" + msg)
+	})
+	PB.Publish("tech","vr is cool!")
+	PB.Publish("cars","they get you goin")
+
+
+	// Concurrent version
+	PBC := NewPubSubConCur()
+	var wg sync.WaitGroup
+	wg.Add(2)
+	sub1 := PBC.Subscribe("tech")
+	sub2 := PBC.Subscribe("cars")
+	ctx1, _ := context.WithTimeout(context.Background(),time.Second * 5)
+	ctx2, _ := context.WithTimeout(context.Background(),time.Second * 5)
+	go func(ctx context.Context){
+		for {
+			select{
+				case v1 := <- sub1:
+					fmt.Println("sub1 concurrent message:",v1)
+				case <-ctx.Done():
+					wg.Done()
+					return
+				}
+		}
+		
+	}(ctx1)
+	
+	go func(ctx context.Context){
+		for {
+			select{
+				case v2 := <- sub2:
+					fmt.Println("sub2 concurrent message:",v2)
+				case <-ctx.Done():
+					wg.Done()
+					return
+			}
+		}
+	}(ctx2)
+	
+	
+	PBC.Publish("tech", "vr is cool!")
+	PBC.Publish("cars", "cars are cool!")
+	wg.Wait()
+	
+}
